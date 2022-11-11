9e0eaeca29eab9e5a572efd4e7cd78733e13ab09
gahyeon noh <rkgus758@gmail.com>
2022-11-11 16:24:06 +0900

    spring boot 다운 (기본셋팅x)

---
+++ src/main/java/com/elleKorclonecoding221111/elleKorclonecoding/ElleKorClonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.elleKorclonecoding221111.elleKorclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElleKorClonecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElleKorClonecodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elleKorclonecoding221111/elleKorclonecoding/ElleKorClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elleKorclonecoding221111.elleKorclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElleKorClonecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
